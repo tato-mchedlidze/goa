d8ad74b914a323ebd8c454d6a4b184c5cff03aa8
tato-mchedlidze <tatomchedlidze210@gmail.com>
2024-10-11 13:35:27 +0400

    second commit

---
+++ day02/classwork/classworl.py
@@ -0,0 +1,11 @@
+print ("tato")
+print ("mchedlidze")
+print ("15")
+
+name= "tato"
+surname= "mchedlidze"
+age= 15
+
+print (name)
+print (surname)
+print (age)
---
+++ day02/homework/print usige.py
@@ -0,0 +1,31 @@
+name = "tato."
+surname = "mchedlidze."
+age = "15"
+name2 = "saba"
+
+print("hallo")
+print("my")
+print("name")
+print("is")
+print(name)
+
+
+print ("i")
+print ("am")
+print (age)
+print("years")
+print("old.")
+
+
+print ("my")
+print ("surname")
+print ("is")
+print (surname)
+
+print ("my")
+print("brothers")
+print ("name")
+print ("is")
+print(name2)
+print ("and")
+print (surname)
