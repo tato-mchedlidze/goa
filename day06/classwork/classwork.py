17e0098edd725010bea59262fce1280d0dbe3e97
tato-mchedlidze <tatomchedlidze210@gmail.com>
2024-10-26 20:24:01 +0400

    second commit

---
+++ day06/classwork/classwork.py
@@ -0,0 +1,64 @@
+x = 10
+y = 15
+z = 20
+b = 25
+g = 30
+x += 10
+y += 15
+z += 20
+b += 25
+g +=30
+print(x)
+print(y)
+print(z)
+print(b)
+print(g)
+
+x1 = 35
+y1 = 40
+z1 = 45
+b1 = 50
+g1 = 55
+x1 -= 10
+y1 -= 15
+z1 -= 20
+b1 -= 25
+g1 -=30
+print(x1)
+print(y1)
+print(z1)
+print(b1)
+print(g1) 
+
+x2 = 60
+y2 = 65
+z2 = 70
+b2 = 75
+g2 = 80
+x2 /= 10
+y2 /= 15
+z2 /= 20
+b2 /= 25
+g2 /=30
+print(x2)
+print(y2)
+print(z2)
+print(b2)
+print(g2)
+
+x3 = 85
+y3 = 90
+z3 = 95
+b3 = 100
+g3 = 105
+x3 *= 10
+y3 *= 15
+z3 *= 20
+b3 *= 25
+g3 *= 30
+print(x3)
+print(y3)
+print(z3)
+print(b3)
+print(g3)
+
---
+++ day06/classwork/classwork1.py
@@ -0,0 +1,30 @@
+print = "Hello World"
+variable = 0.5
+print (variable)
+
+print = ("Hello World")
+print = ("This is an indented line")
+
+undeclared_variable = 2.4
+print = (undeclared_variable)
+
+
+c = 5
+print = (c)
+
+print = ("Hello World")
+
+x = 10
+y = 5
+print(x + y)
+
+print("Missing indentation in function")
+
+var = 10
+print = (var)
+
+a, b = 10
+print(a, b)
+
+print("Unclosed string")
+print("Escaped quote:" )
