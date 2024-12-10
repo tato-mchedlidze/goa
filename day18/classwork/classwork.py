f227c7248207b715e1b5d389cde7e884ef9b3dd9
tato-mchedlidze <tatomchedlidze210@gmail.com>
2024-12-10 16:48:03 +0400

    second commit

---
+++ day18/classwork/classwork.py
@@ -0,0 +1,15 @@
+i = 2
+while i < 20:
+    print(i)
+    i += 2
+
+i = 3
+while i < 20:
+    print(i)
+    i += 3
+
+for i in range(10, 31):
+    if i%2 ==0:
+        print(str(i), " - even")
+    else:
+        print(str(i), " - odd")
---
+++ day18/classwork/classwork1.py
@@ -0,0 +1,3 @@
+num1 = input("num1:")
+num2 = input("num2:")
+
